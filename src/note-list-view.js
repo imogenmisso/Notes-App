0a294ac8d37741f282476cfefe1137141cd29a6e
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-15 14:56:53 +0000

    update

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,20 +1,20 @@
 (function(exports) {
   function NoteListView(noteList) {
-    this.noteList = noteList
+    this.noteList = noteList;
   }
 
   NoteListView.prototype.getHTML = function() {
-    var view = '<ul>'
+    var view = '<ul>';
     this.noteList.getNotes().forEach(function(note) {
       if (note.text.length > 20) {
-        view += '<li><div><a href="http://localhost:8080#notes/' + note.id + '">' + note.text.slice(0, 20) + '...</a></div></li>'
+        view += '<li><div><a href="http://localhost:8080#notes/' + note.id + '">' + note.text.slice(0, 20) + '...</a></div></li>';
       } else {
-        view += '<li><div><a href="http://localhost:8080#notes/' + note.id + '">' + note.text + '</a></div></li>'
+        view += '<li><div><a href="http://localhost:8080#notes/' + note.id + '">' + note.text + '</a></div></li>';
       }
-      })
-      view += '</ul>'
-      return view
-  }
+    });
+      view += '</ul>';
+      return view;
+  };
 
-  exports.NoteListView = NoteListView
+  exports.NoteListView = NoteListView;
 })(this);
